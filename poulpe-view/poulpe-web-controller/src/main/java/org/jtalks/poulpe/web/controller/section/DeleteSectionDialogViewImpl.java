be6b5821b9213940f8f6e444691ba580b92746bf
kot <bdk.free@gmail.com>
2011-08-27 22:05:05 +0700

    POULPE-31 UI for delete section implemented

---
+++ poulpe-view/poulpe-web-controller/src/main/java/org/jtalks/poulpe/web/controller/section/DeleteSectionDialogViewImpl.java
@@ -0,0 +1,111 @@
+/**
+ * Copyright (C) 2011  jtalks.org Team
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * Also add information on how to contact you by electronic and paper mail.
+ * Creation date: Apr 12, 2011 / 8:05:19 PM
+ * The jtalks.org Project
+ */
+package org.jtalks.poulpe.web.controller.section;
+
+
+import java.util.List;
+
+import org.jtalks.poulpe.model.entity.Section;
+import org.zkoss.zk.ui.Components;
+import org.zkoss.zk.ui.ext.AfterCompose;
+import org.zkoss.zul.Combobox;
+import org.zkoss.zul.Comboitem;
+import org.zkoss.zul.ComboitemRenderer;
+import org.zkoss.zul.ListModelList;
+import org.zkoss.zul.Window;
+import org.zkoss.zul.api.Radiogroup;
+
+public class DeleteSectionDialogViewImpl extends Window implements DeleteSectionDialogView, AfterCompose {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -4999382692611273729L;
+    private Radiogroup deleteMode;
+    private Combobox   selectedSection;
+    private Section    deletedSection;
+    private DeleteSectionDialogPresenter presenter;
+    
+    public void setPresenter(DeleteSectionDialogPresenter presenter){
+      this.presenter = presenter;      
+    }
+    
+    @Override
+    public void afterCompose() {
+        Components.wireVariables(this, this);
+        Components.addForwards(this, this);
+        
+        presenter.setView(this);
+      
+       selectedSection.setItemRenderer(new ComboitemRenderer() {
+       
+        @Override
+        public void render(Comboitem item, Object data) throws Exception {
+            Section section = (Section)data;
+            item.setLabel(section.getName());
+        }
+    });
+       
+    }
+    
+    
+    @Override
+    public Section getDeleteSection() {
+        return deletedSection;
+    }
+
+    @Override
+    public Section getSelectedSection() {
+        return (Section)selectedSection.getModel().getElementAt( selectedSection.getSelectedIndex());
+    }
+
+    @Override
+    public String getDeleteMode() {
+        return deleteMode.getItemAtIndexApi( deleteMode.getSelectedIndex() ).getValue();
+    }
+
+    @Override
+    public void closeDialog() {
+        setVisible(false);
+    }
+
+    @Override
+    public void showDialog(List<Section> selectableSections) {
+        selectedSection.setModel(new ListModelList( selectableSections ));
+        setVisible(true);
+    }
+
+    public void onClick$confirmButton(){
+        presenter.delete();
+    }
+    
+    public void onClick$rejectButton(){
+        closeDialog();
+    }
+    
+    public void onCheck$removeAndMoveMode(){
+        selectedSection.setVisible(true);
+        selectedSection.setSelectedIndex(0);
+    }
+    
+    public void onCheck$deleteAllMode(){
+        selectedSection.setVisible(false);
+        selectedSection.setSelectedIndex(-1);
+    }
+
+}
