abc2a128ec3f048116ecbae758b035d029e4518d
ctapobep <ctapobep@ctapobep-K52Jr.(none)>
2011-12-17 19:21:22 +0200

    #POULPE-100 Wrote JavaDocs and refactored the BasicAclBuilder and UserGroupSid

--- poulpe-service/src/main/java/org/jtalks/poulpe/service/security/BasicAclBuilder.java
+++ poulpe-service/src/main/java/org/jtalks/poulpe/service/security/BasicAclBuilder.java
@@ -1,60 +1,106 @@
 package org.jtalks.poulpe.service.security;
 
+import com.google.common.collect.Lists;
 import org.jtalks.common.model.entity.Entity;
 import org.jtalks.common.model.entity.User;
 import org.jtalks.poulpe.model.entity.Group;
 import org.jtalks.poulpe.service.movetocommon.AclManager;
 import org.jtalks.poulpe.service.movetocommon.JtalksPermission;
-import org.springframework.security.acls.domain.GrantedAuthoritySid;
 import org.springframework.security.acls.domain.PrincipalSid;
 import org.springframework.security.acls.model.Permission;
 import org.springframework.security.acls.model.Sid;
+import ru.javatalks.utils.general.Assert;
 
 import javax.annotation.Nonnull;
+import javax.annotation.concurrent.NotThreadSafe;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
+ * This builder is dedicated to easily define what objects ({@link Sid}s) will own {@link Permission}s to do actions on
+ * domain objects ({@link #on(Entity)}. <br/> If you want to add handy methods (e.g. fluent methods for giving
+ * permissions), extend this class.
+ *
  * @author stanislav bashkirstsev
+ * @see <a href="http://static.springsource.org/spring-security/site/docs/3.0.x/reference/domain-acls.html">Spring
+ *      ACL documentation</a>
  */
-//TODO: this is a draft, it's still need to be finished
+@NotThreadSafe
 public class BasicAclBuilder {
-    /**
-     * Possible actions that will be performed when builder finished.
-     */
-    public enum Action {
-        DELETE, GRANT, REVOKE
-    }
-
-    private final List<Sid> sids = new ArrayList<Sid>();
-    private final List<Permission> permissions = new ArrayList<Permission>();
-    private final List<Permission> unmodifiablePermissions = Collections.unmodifiableList(permissions);
+    private final List<Permission> permissionsToRevoke = new ArrayList<Permission>();
+    private final List<Permission> permissionsToGrant = new ArrayList<Permission>();
     private final AclManager aclManager;
-    private final Action action;
     private Entity target;
+    private Sid sid;
 
     /**
-     * Constructor.
+     * Constructs the builder with mandatory {@link AclManager}, builder will delegate the saving of its state to the
+     * acl manager at some point.
      *
-     * @param aclManager instance of manager
-     * @param action     action that will be executed when you call {@link org.jtalks.poulpe.service.movetocommon.AclBuilder#on(Entity)
+     * @param aclManager the ACL manager that will be used to save the state of the builder to the DB
      */
-    public BasicAclBuilder(AclManager aclManager, Action action) {
+    public BasicAclBuilder(@Nonnull AclManager aclManager) {
+        Assert.throwIfNull(aclManager, "acl manager");
         this.aclManager = aclManager;
-        this.action = action;
     }
 
-    public BasicAclBuilder setOwner(@Nonnull User owner){
-        sids.add(new PrincipalSid(owner.getUsername()));
+    /**
+     * Sets the user that will be granted (or revoked from) the permissions.
+     *
+     * @param owner the user to grant to/revoke from the permissions
+     * @return this
+     */
+    public BasicAclBuilder setOwner(@Nonnull User owner) {
+        sid = new PrincipalSid(owner.getUsername());
         return this;
     }
 
-    public BasicAclBuilder setOwner(@Nonnull Group owner){
-        sids.add((new UserGroupSid(owner)));
+    /**
+     * Sets the user group that will be granted (or revoke from) the permissions. Thus, all the users in that group will
+     * be granted as well.
+     *
+     * @param owner the user group that is the owner of the permissions
+     * @return this
+     */
+    public BasicAclBuilder setOwner(@Nonnull Group owner) {
+        sid = new UserGroupSid(owner);
         return this;
     }
 
+    /**
+     * Adds a permission to the list of {@link Sid}s permissions, in other words grants the permission to the user (even
+     * if it effectively is a restriction, it's still 'granting' since a restriction is described with the same class as
+     * regular permission).
+     *
+     * @param permission the permission to grant to the owner ({@link Sid})
+     * @return this
+     */
+    public BasicAclBuilder grant(@Nonnull JtalksPermission permission) {
+        permissionsToGrant.add(permission);
+        return this;
+    }
+
+    /**
+     * Adds a permission to the list of {@link Sid}s permissions, in other words grants the permission to the user (even
+     * if it effectively is a restriction, it's still 'granting' since a restriction is described with the same class as
+     * regular permission).
+     *
+     * @param permission the permission to grant to the owner ({@link Sid})
+     * @return this
+     */
+    public BasicAclBuilder revoke(@Nonnull JtalksPermission permission) {
+        permissionsToRevoke.add(permission);
+        return this;
+    }
+
+    /**
+     * Performs updates (flushes the state to the {@link AclManager} which should save everything to DB) and clears the
+     * state of the builder so that you can reuse it for next objects.
+     *
+     * @param object an object the permission is given on. So {@link Sid}s have {@link Permission}s to do something on
+     *               this object.
+     * @return this
+     */
     public BasicAclBuilder on(Entity object) {
         throwIfPermissionsOrSidsEmpty();
         target = object;
@@ -62,55 +108,45 @@ public class BasicAclBuilder {
         return this;
     }
 
-    public BasicAclBuilder addPermission(JtalksPermission permission) {
-        permissions.add(permission);
-        return this;
+    /**
+     * Flushes the state to the {@link AclManager} (which in turns should flush everything to DB) and then clears the
+     * state.
+     */
+    private void executeUpdate() {
+        executeGrant(permissionsToGrant);
+        executeRevoke(permissionsToRevoke);
+        clearState();
     }
 
-    private void throwIfPermissionsOrSidsEmpty() {
-        if (sids.isEmpty() || permissions.isEmpty()) {
-            throw new IllegalStateException("You can't grant permissions without sids or permissions");
+    private void executeRevoke(List<Permission> permissionsToRevoke) {
+        if (!permissionsToRevoke.isEmpty()) {
+            aclManager.delete(Lists.newArrayList(sid), permissionsToRevoke, target);
+        }
+    }
+
+    private void executeGrant(List<Permission> permissionsToGrant) {
+        if (!permissionsToGrant.isEmpty()) {
+            aclManager.grant(Lists.newArrayList(sid), permissionsToGrant, target);
         }
     }
 
     /**
-     * Performs selected action.
+     * Clears the state of the builder (cleans all the permissions, sids, target) so that the object can be reused.
+     *
+     * @return this
      */
-    private void executeUpdate() {
-        if (action == Action.GRANT) {
-            aclManager.grant(sids, permissions, target);
-        } else if (action == Action.REVOKE) {
-            aclManager.revoke(sids, permissions, target);
-        } else {
-            aclManager.delete(sids, permissions, target);
+    public BasicAclBuilder clearState() {
+        permissionsToGrant.clear();
+        permissionsToRevoke.clear();
+        target = null;
+        sid = null;
+        return this;
+    }
+
+    private void throwIfPermissionsOrSidsEmpty() throws IllegalArgumentException {
+        if (sid == null || (permissionsToGrant.isEmpty() || permissionsToRevoke.isEmpty())) {
+            throw new IllegalStateException("You can't grant permissions without sids or permissions");
         }
-        sids.clear();
-        permissions.clear();
     }
 
-    public boolean containsSid(String name) {
-        for (Sid sid : sids) {
-            if (sid instanceof PrincipalSid) {
-                if (((PrincipalSid) sid).getPrincipal().equals(name)) {
-                    return true;
-                }
-            } else if (((GrantedAuthoritySid) sid).getGrantedAuthority().equals(name)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    public boolean hasPermission(Permission permission) {
-        return permissions.contains(permission);
-    }
-
-    public List<Sid> getSids() {
-        return new ArrayList<Sid>(sids);
-    }
-
-    public List<Permission> getPermissions() {
-        return unmodifiablePermissions;
-    }
 }
--- poulpe-service/src/main/java/org/jtalks/poulpe/service/security/UserGroupSid.java
+++ poulpe-service/src/main/java/org/jtalks/poulpe/service/security/UserGroupSid.java
@@ -3,18 +3,36 @@ package org.jtalks.poulpe.service.security;
 import org.jtalks.poulpe.model.entity.Group;
 import org.springframework.security.acls.model.Sid;
 
+import javax.annotation.Nonnegative;
 import javax.annotation.Nonnull;
 
 /**
+ * This class does the same as {@link org.springframework.security.acls.domain.PrincipalSid} does for users. More
+ * precisely it contains some security ID that is used by Spring ACL to associate the owner to the permissions it owns.
+ * Thus this class contains an ID that will be saved into database ({@code acl_sids#sid, acl_object_identity#owner_sid,
+ * acl_entry#sid}).
+ *
  * @author stanislav bashkirstev
  */
 public class UserGroupSid implements Sid {
     private final String groupId;
 
-    public UserGroupSid(@Nonnull Long groupId) {
+    /**
+     * Constructs SID by the group id.
+     *
+     * @param groupId the id of the group that will own permissions for some actions on some objects
+     * @see Group#getId()
+     */
+    public UserGroupSid(@Nonnegative long groupId) {
         this.groupId = String.valueOf(groupId);
     }
 
+    /**
+     * Constructs a SID by retrieving the group id from the {@link Group} object.
+     *
+     * @param group a group to take its database id
+     * @see Group#getId()
+     */
     public UserGroupSid(@Nonnull Group group) {
         this.groupId = String.valueOf(group.getId());
     }
