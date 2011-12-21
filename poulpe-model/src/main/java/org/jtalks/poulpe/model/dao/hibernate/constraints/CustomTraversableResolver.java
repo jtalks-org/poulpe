2fa671f27db89abccad12cf1cc60787c620f6833
stolzen <alexey.s.grigoriev@gmail.com>
2011-12-21 21:02:07 +0300

    modified the branch class

--- poulpe-model/src/main/java/org/jtalks/poulpe/model/dao/hibernate/constraints/CustomTraversableResolver.java
+++ poulpe-model/src/main/java/org/jtalks/poulpe/model/dao/hibernate/constraints/CustomTraversableResolver.java
@@ -4,15 +4,16 @@ import java.lang.annotation.ElementType;
 import javax.validation.Path;
 import javax.validation.TraversableResolver;
 
+// TODO: what is it for?
 public class CustomTraversableResolver implements TraversableResolver {
 
 	public boolean isReachable(Object traversableObject,
-			Path.Node traversableProperty, Class rootBeanType,
+			Path.Node traversableProperty, Class<?> rootBeanType,
 			Path pathToTraversableObject, ElementType elementType) {
 		return true;
 	}
 
-	public boolean isCascadable(Object object, Path.Node node, Class c,
+	public boolean isCascadable(Object object, Path.Node node, Class<?> c,
 			Path path, ElementType elementType) {
 		return true;
 	}
--- poulpe-model/src/main/java/org/jtalks/poulpe/model/dao/hibernate/constraints/UniqueConstraintValidator.java
+++ poulpe-model/src/main/java/org/jtalks/poulpe/model/dao/hibernate/constraints/UniqueConstraintValidator.java
@@ -1,82 +1,106 @@
 package org.jtalks.poulpe.model.dao.hibernate.constraints;
 
-import static org.hibernate.criterion.DetachedCriteria.forClass;
-import static org.hibernate.criterion.Projections.count;
-import static org.hibernate.criterion.Restrictions.eq;
-
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.beanutils.BeanUtils;
 import org.hibernate.Criteria;
 import org.hibernate.SessionFactory;
 import org.hibernate.classic.Session;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
+import org.jtalks.common.model.entity.Entity;
 
-public class UniqueConstraintValidator implements
-		ConstraintValidator<Unique, String> {
-	
-	private Class<?> entity;
-	private String field;
+/**
+ * TODO: add javadocs
+ * 
+ * @author Tatiana Birina
+ * @author Alexey Grigorev
+ * 
+ * @see <a
+ * href="http://stackoverflow.com/questions/1972933/cross-field-validation-with-hibernate-validator-jsr-303">idea</a>
+ */
+public class UniqueConstraintValidator implements ConstraintValidator<UniqueFields, Entity> {
 
-	/**
-	 * retrieves field to check and Class instance
-	 */
-	public void initialize(Unique annotation) {
-		this.entity = annotation.entity();
-		this.field = annotation.field();
-	}
+    /**
+     * Hibernate SessionFactory
+     */
+    private SessionFactory sessionFactory;
 
-	/**
-	 * determines if the value is unique
-	 * 
-	 * @return true if no duplicates are found false if there are duplicates or
-	 *         value is empty
-	 */
-	public boolean isValid(String value, ConstraintValidatorContext context) {
-		if (StringUtils.isEmpty(value)) {
-			return false;
-		}
-		return query(value).intValue() == 0;
-	}
+    private String[] fields;
 
-	private Number query(String value) {
-		Criteria criteria = getSession().createCriteria(entity);
-		criteria.add(Restrictions.eq(field, value));
-		criteria.setProjection(Projections.rowCount());
-		Number count = (Number) criteria.list().get(0);
-		return count;
-	}
+    /**
+     * retrieves field to check and Class instance
+     */
+    @Override
+    public void initialize(UniqueFields annotation) {
+        fields = annotation.fields();
+    }
 
-	/**
-	 * Hibernate SessionFactory
-	 */
-	private SessionFactory sessionFactory;
+    /**
+     * determines if the value is unique
+     * 
+     * @return true if no duplicates are found false if there are duplicates or
+     * value is empty
+     */
+    @Override
+    public boolean isValid(Entity entity, ConstraintValidatorContext context) {
+        // TODO: for making test 'validateBranchNameFieldViolated' work,
+        // builder from context may be used, further investigation needed
+        // TODO: the same approach as in ComponentDuplicatesFinder might be used for
+        // building a set of violated fields
+        Number result = retrieveDuplicatesCount(entity);
+        boolean violated = result.intValue() == 0;
+        return violated;
+    }
 
-	/**
-	 * Get current Hibernate session.
-	 * 
-	 * @return current Session
-	 */
-	// TODO: if should be removed after issue with
-	// sessionFactory(NullPointer exception) will be resolved
-	protected Session getSession() {
-		Session ses = null;
-		if (sessionFactory != null) {
-			ses = sessionFactory.getCurrentSession();
-		}
-		return ses;
-	}
+    private Number retrieveDuplicatesCount(Entity entity) {
+        Criteria criteria = getSession().createCriteria(entity.getClass());
 
-	/**
-	 * Setter for Hibernate SessionFactory.
-	 * 
-	 * @param sessionFactory
-	 *            the sessionFactory to set
-	 */
-	public void setSessionFactory(SessionFactory sessionFactory) {
-		this.sessionFactory = sessionFactory;
-	}
+        for (String field : fields) {
+            String property = extractField(entity, field);
+            if (property != null) {
+                criteria.add(Restrictions.eq(field, property));
+            }
+        }
+
+        criteria.add(Restrictions.ne("id", entity.getId()));
+
+        criteria.setProjection(Projections.rowCount());
+
+        Number result = (Number) criteria.uniqueResult();
+        return result;
+    }
+
+    private String extractField(Entity entity, String field) {
+        try {
+            return BeanUtils.getProperty(entity, field);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Get current Hibernate session.
+     * 
+     * @return current Session
+     */
+    protected Session getSession() {
+        return sessionFactory.getCurrentSession();
+    }
+
+    /**
+     * @return the sessionFactory
+     */
+    public SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    /**
+     * @param sessionFactory the sessionFactory to set
+     */
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
 }
