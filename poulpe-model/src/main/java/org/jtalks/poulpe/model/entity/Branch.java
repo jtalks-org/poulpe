007f5f00f42a80f2f049dccdcaa00d6706f5e74f
Necros <n.c.necros@gmail.com>
2012-01-05 13:52:07 +0400

    #POULPE-139; Added missed JavaDoc for Branch

--- poulpe-model/src/main/java/org/jtalks/poulpe/model/entity/Branch.java
+++ poulpe-model/src/main/java/org/jtalks/poulpe/model/entity/Branch.java
@@ -20,6 +20,7 @@ import java.util.Collections;
 import java.util.List;
 
 import javax.validation.constraints.NotNull;
+
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.jtalks.common.model.entity.Entity;
@@ -29,35 +30,48 @@ import org.jtalks.poulpe.validation.annotations.UniqueField;
 
 /**
  * Forum branch that contains topics related to branch theme.
- * 
+ *
  * @author Pavel Vervenko
  */
 @UniqueConstraint
 public class Branch extends Entity {
-    
+
     public static final String BRANCH_ALREADY_EXISTS = "branches.error.branch_name_already_exists";
     public static final String BRANCH_CANT_BE_VOID = "branches.error.branch_name_cant_be_void";
     public static final String ERROR_LABEL_SECTION_NAME_WRONG = "sections.editsection.name.err";
-    
+
     @UniqueField(message = BRANCH_ALREADY_EXISTS)
     @NotNull(message = BRANCH_CANT_BE_VOID)
     @NotEmpty(message = BRANCH_CANT_BE_VOID)
     @Length(min = 1, max = 254, message = ERROR_LABEL_SECTION_NAME_WRONG)
     private String name;
-    
     private String description;
     private boolean deleted;
     private Section section;
-    
+
     private List<User> moderators = new ArrayList<User>();
 
+    /**
+     * Default constructor
+     */
     public Branch() {
     }
 
+    /**
+     * Create Branch with name.
+     *
+     * @param name - name for branch
+     */
     public Branch(String name) {
         this.name = name;
     }
 
+    /**
+     * Create Branch with name and description
+     *
+     * @param name        - name for new Branch
+     * @param description - description for new Branch
+     */
     public Branch(String name, String description) {
         this.name = name;
         this.description = description;
@@ -65,6 +79,8 @@ public class Branch extends Entity {
 
     /**
      * Get branch name which briefly describes the topics contained in it.
+     *
+     * @return Branch name as String
      */
     public String getName() {
         return name;
@@ -72,6 +88,8 @@ public class Branch extends Entity {
 
     /**
      * Set branch name.
+     *
+     * @param name - Branch name
      */
     public void setName(String name) {
         this.name = name;
@@ -79,6 +97,8 @@ public class Branch extends Entity {
 
     /**
      * Get branch description.
+     *
+     * @return Branch description
      */
     public String getDescription() {
         return description;
@@ -87,6 +107,8 @@ public class Branch extends Entity {
     /**
      * Set branch description which contains additional information about the
      * branch.
+     *
+     * @param description - Branch description
      */
     public void setDescription(String description) {
         this.description = description;
@@ -94,6 +116,7 @@ public class Branch extends Entity {
 
     /**
      * Check if branch marked is deleted.
+     *
      * @return true if is deleted, false otherwise
      */
     public boolean getDeleted() {
@@ -102,6 +125,8 @@ public class Branch extends Entity {
 
     /**
      * Mark branch as deleted. True means that branch is deleted.
+     *
+     * @param deleted - boolean flag.
      */
     public void setDeleted(boolean deleted) {
         this.deleted = deleted;
@@ -109,6 +134,7 @@ public class Branch extends Entity {
 
     /**
      * Returns the section in which this branch is.
+     *
      * @return the parent section
      */
     public Section getSection() {
@@ -117,6 +143,7 @@ public class Branch extends Entity {
 
     /**
      * Sets the section in which this branch is.
+     *
      * @param section the parent section
      */
     public void setSection(Section section) {
@@ -125,7 +152,7 @@ public class Branch extends Entity {
 
     /**
      * @return an unmodifiable list of {@link User} which are signed to moderate
-     * this branch
+     *         this branch
      */
     public List<User> getModeratorsList() {
         return Collections.unmodifiableList(moderators);
@@ -133,6 +160,7 @@ public class Branch extends Entity {
 
     /**
      * Protected for using only by hibernate.
+     *
      * @return the list of moderators.
      */
     protected List<User> getModerators() {
@@ -142,6 +170,7 @@ public class Branch extends Entity {
     /**
      * Sets the list of users which will be signed to moderate this branch.
      * Protected for using only by hibernate.
+     *
      * @param moderators a list of {@link User}
      */
     protected void setModerators(List<User> moderators) {
@@ -157,6 +186,8 @@ public class Branch extends Entity {
 
     /**
      * Assigns a list of {@link User} to moderate this branch
+     *
+     * @param users - list of users
      */
     public void addModerators(List<User> users) {
         this.moderators.addAll(users);
@@ -165,6 +196,8 @@ public class Branch extends Entity {
     /**
      * Assigns a list of {@link User} to moderate this branch. This method
      * mainly for using in test.
+     *
+     * @param users - list of users
      */
     public void addModerators(User... users) {
         this.addModerators(Arrays.asList(users));
@@ -179,6 +212,8 @@ public class Branch extends Entity {
 
     /**
      * Checks if {@link User} is assigned to moderate this branch
+     *
+     * @param user - user for check
      * @return {@code true} if assigned, {@code false} otherwise
      */
     public boolean isModeratedBy(User user) {
