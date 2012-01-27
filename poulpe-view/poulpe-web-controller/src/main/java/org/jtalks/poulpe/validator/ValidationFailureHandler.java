7e2c88092c35676db1442349b6f3a0b1850eb314
stolzen <alexey.s.grigoriev@gmail.com>
2012-01-27 15:17:59 +0300

    Changed the ValidationHandler so now it has a state

--- poulpe-view/poulpe-web-controller/src/main/java/org/jtalks/poulpe/validator/ValidationFailureHandler.java
+++ poulpe-view/poulpe-web-controller/src/main/java/org/jtalks/poulpe/validator/ValidationFailureHandler.java
@@ -10,20 +10,41 @@ import org.zkoss.zul.impl.InputElement;
 
 import com.google.common.collect.ImmutableMap;
 
-public class ValidationFailureHandler {
-	
-	public void validationFailure(ValidationResult result, String textBoxName, Textbox textBox) {
-        Map<String, ? extends InputElement> comps = ImmutableMap.of(textBoxName, textBox);
-        
+/**
+ * @author Alexey Grigorev
+ * @author Tatiana Birina
+ */
+public class ValidationFailureHandler implements ValidationFailure {
+
+    private final Map<String, ? extends InputElement> comps;
+
+    public ValidationFailureHandler(String name, Textbox textBox) {
+        comps = ImmutableMap.of(name, textBox);
+    }
+
+    public ValidationFailureHandler(String name1, Textbox textBox1, String name2, Textbox textBox2) {
+        comps = ImmutableMap.of(name1, textBox1, name2, textBox2);
+    }
+
+    public ValidationFailureHandler(String name1, Textbox textBox1, String name2, Textbox textBox2, String name3,
+            Textbox textBox3) {
+        comps = ImmutableMap.of(name1, textBox1, name2, textBox2, name3, textBox3);
+    }
+
+    @Override
+    public void validationFailure(ValidationResult result) {
         for (ValidationError error : result.getErrors()) {
             String fieldName = error.getFieldName();
             if (comps.containsKey(fieldName)) {
-                String message = Labels.getLabel(error.getErrorMessageCode());
-
-                InputElement ie = comps.get(fieldName);
-                ie.setErrorMessage(message);
+                showErrorMessage(error, fieldName);
             }
         }
+    }
+
+    private void showErrorMessage(ValidationError error, String fieldName) {
+        String message = Labels.getLabel(error.getErrorMessageCode());
+        InputElement ie = comps.get(fieldName);
+        ie.setErrorMessage(message);
     }
 
 }
