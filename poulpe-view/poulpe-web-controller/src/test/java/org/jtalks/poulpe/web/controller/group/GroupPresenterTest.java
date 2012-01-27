88627f559518988a4b4c1ed753899e73cf5d2602
stolzen <alexey.s.grigoriev@gmail.com>
2012-01-27 16:47:16 +0300

    Some minor changes

--- poulpe-view/poulpe-web-controller/src/test/java/org/jtalks/poulpe/web/controller/group/GroupPresenterTest.java
+++ poulpe-view/poulpe-web-controller/src/test/java/org/jtalks/poulpe/web/controller/group/GroupPresenterTest.java
@@ -15,9 +15,8 @@
 package org.jtalks.poulpe.web.controller.group;
 
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
-import java.util.LinkedList;
+import java.util.Collections;
 import java.util.List;
 
 import org.jtalks.poulpe.model.entity.Group;
@@ -26,28 +25,14 @@ import org.jtalks.poulpe.web.controller.DialogManager;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class GroupPresenterTest {
     GroupPresenter presenter;
-    @Mock
-    DialogManager dialogManager;
-    @Mock
-    GroupService service;
-    @Mock
-    GroupViewImpl viewMock;
-
-    @Test(dataProvider = "emptyListProvider")
-    public void testInitView(List<Group> returnedGroups) throws Exception {
-        when(service.getAllMatchedByName(null)).thenReturn(returnedGroups);
-        verify(viewMock).updateView(returnedGroups);
-    }
-
-    @DataProvider(name = "emptyListProvider")
-    public Object[][] testData() {
-        return new Object[][] { { new LinkedList() } };
-    }
+    
+    @Mock DialogManager dialogManager;
+    @Mock GroupService service;
+    @Mock GroupViewImpl viewMock;
 
     @BeforeMethod
     public void beforeMethod() {
@@ -57,7 +42,13 @@ public class GroupPresenterTest {
         presenter.setGroupService(service);
         presenter.initView(viewMock);
     }
-
+    
+    @Test
+    public void testInitView() throws Exception {
+        List<Group> emptyList = Collections.emptyList();
+        verify(viewMock).updateView(emptyList);
+    }
+    
     @Test
     public void testOnEditGroup() {
         Group group = new Group();
