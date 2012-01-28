6891bcc9f313b3cd812ba1923745df5b64aff2cf
stolzen <alexey.s.grigoriev@gmail.com>
2012-01-28 18:02:19 +0300

    Moved tests related to validator in section presenter test into separate test class

---
+++ poulpe-view/poulpe-web-controller/src/test/java/org/jtalks/poulpe/web/controller/section/SectionPresenterTestValidation.java
@@ -0,0 +1,98 @@
+package org.jtalks.poulpe.web.controller.section;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyBoolean;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Collections;
+import java.util.Set;
+
+import org.jtalks.poulpe.model.entity.Section;
+import org.jtalks.poulpe.service.SectionService;
+import org.jtalks.poulpe.validation.EntityValidator;
+import org.jtalks.poulpe.validation.ValidationError;
+import org.jtalks.poulpe.validation.ValidationResult;
+import org.jtalks.poulpe.web.controller.DialogManager;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+/**
+ * @author Tatiana Birina
+ * @author Grigorev Alexey
+ */
+public class SectionPresenterTestValidation {
+
+    private SectionPresenter presenter;
+
+    @Mock SectionService service;
+    @Mock SectionViewImpl view;
+    @Mock DialogManager dialogManager;
+    @Mock EntityValidator entityValidator;
+
+    private ValidationResult resultWithErrors = resultWithErrors();
+
+    @BeforeMethod
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        
+        presenter = new SectionPresenter();
+
+        presenter.setSectionService(service);
+        presenter.setDialogManager(dialogManager);
+        presenter.setEntityValidator(entityValidator);
+
+        presenter.initView(view);
+    }
+
+    private ValidationResult resultWithErrors() {
+        ValidationError error = new ValidationError("name", Section.SECTION_ALREADY_EXISTS);
+        Set<ValidationError> errors = Collections.singleton(error);
+        return new ValidationResult(errors);
+    }
+
+    @Test
+    public void testValidateSectionEmptyName() {
+        givenConstraintViolated();
+        presenter.addNewSection("", "description");
+        verify(service, never()).saveSection(any(Section.class));
+    }
+
+    @Test
+    public void testValidateSectionNullName() {
+        givenConstraintViolated();
+        presenter.addNewSection(null, "description");
+        verify(service, never()).saveSection(any(Section.class));
+    }
+
+    @Test
+    public void testCheckSectionUniquenessOK() {
+        givenNoConstraintsViolated();
+        
+        presenter.addNewSection("name", "description");
+        
+        verify(view, never()).validationFailure(any(ValidationResult.class), anyBoolean());
+        verify(dialogManager).confirmCreation(anyString(), any(DialogManager.Performable.class));
+    }
+
+    private void givenNoConstraintsViolated() {
+        when(entityValidator.validate(any(Section.class))).thenReturn(ValidationResult.EMPTY);
+    }
+
+    @Test
+    public void testCheckSectionUniquenessNonUnique() {
+        givenConstraintViolated();
+        presenter.addNewSection("name", "description");
+        verify(service, never()).saveSection(any(Section.class));
+        verify(dialogManager, never()).confirmCreation(anyString(), any(DialogManager.Performable.class));
+    }
+
+    private void givenConstraintViolated() {
+        when(entityValidator.validate(any(Section.class))).thenReturn(resultWithErrors);
+    }
+}
