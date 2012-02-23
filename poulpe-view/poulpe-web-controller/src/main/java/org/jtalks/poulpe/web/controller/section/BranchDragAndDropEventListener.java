7490875c6428547146d09accbcc065b7454422e7
Guram Savinov <savinov.guram@gmail.com>
2012-02-23 16:14:02 +0300

    #POULPE-82 Added the author annotation.

--- poulpe-view/poulpe-web-controller/src/main/java/org/jtalks/poulpe/web/controller/section/BranchDragAndDropEventListener.java
+++ poulpe-view/poulpe-web-controller/src/main/java/org/jtalks/poulpe/web/controller/section/BranchDragAndDropEventListener.java
@@ -11,6 +11,8 @@ import java.util.List;
 
 /**
  * Listener for Drag'n'Drop event.
+ * 
+ * @author Guram Savinov
  */
 @SuppressWarnings("serial")
 class BranchDragAndDropEventListener extends GenericEventListener<Event> {
