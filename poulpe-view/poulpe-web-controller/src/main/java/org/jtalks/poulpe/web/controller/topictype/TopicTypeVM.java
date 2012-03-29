974479b4fd1f04cfb13982702377821731c0f126
Vahluev Vyacheslav <vahluevvu@gmail.com>
2012-03-29 13:16:35 +0400

    #POULPE-209 Refactoring. * Added Pop-Up Edit and New window. Changes according to ctapobep's comments;  * javadocs are placed before annotations;  * unnecessary NotifyChange are removed  * added tests to support new pop-up window

--- poulpe-view/poulpe-web-controller/src/main/java/org/jtalks/poulpe/web/controller/topictype/TopicTypeVM.java
+++ poulpe-view/poulpe-web-controller/src/main/java/org/jtalks/poulpe/web/controller/topictype/TopicTypeVM.java
@@ -49,17 +49,138 @@ public class TopicTypeVM {
     private DialogManager dialogManager;
     //validator JSR-303
     private EntityValidator entityValidator;
+    //dialogMessage when new window is opened
+    private String editMessage;
+    //messages for creating and editing topicType
+    public static final String NEW_TOPIC_TYPE = "item.add";
+    public static final String EDIT_TOPIC_TYPE = "item.edit";
 
     /**
      * Constructor takes TopicTypeService and DialogManager as its arguments
      *
      * @param topicTypeService {@link TopicTypeService} to use
      * @param dialogManager    {@link DialogManager} to use
+     * @param entityValidator    {@link EntityValidator} to use
      */
     public TopicTypeVM(@Nonnull TopicTypeService topicTypeService, @Nonnull DialogManager dialogManager, @Nonnull EntityValidator entityValidator) {
         this.topicTypeService = topicTypeService;
         this.dialogManager = dialogManager;
         this.entityValidator = entityValidator;
+    }
+
+    //action command
+
+    /**
+     * Creates new TopicType and adds it on form
+     */
+    @NotifyChange({"selected", "topicTypes", "editMessage"})
+    @Command
+    public void newTopicType() {
+        TopicType topicType = new TopicType();
+        //select the new one
+        selected = topicType;
+        selected.setTitle("New Title");
+        selected.setDescription("New Description");
+        getTopicTypes().add(topicType);
+        editMessage = Labels.getLabel(NEW_TOPIC_TYPE);
+    }
+
+    /**
+     * Edits the TopicType selected, shows Dialog Window
+     */
+    @NotifyChange({"selected", "topicTypes", "editMessage"})
+    @Command
+    public void editTopicType() {
+        editMessage = Labels.getLabel(EDIT_TOPIC_TYPE);
+    }
+
+    /**
+     * Saves current TopicType selected. Doesn't save other if changed.
+     * Shows warning or error messages if something is wrong
+     */
+    @NotifyChange({"selected", "topicTypes", "editMessage"})
+    @Command
+    public void saveTopicType() {
+        getTopicTypeService().saveOrUpdate(selected);
+        cancelEditTopicType();
+    }
+
+    /**
+     * Hides window for editing or creating of TopicType
+     */
+    @NotifyChange({"selected", "topicTypes", "editMessage"})
+    @Command
+    public void cancelEditTopicType() {
+        editMessage = null;
+        selected = null;
+    }
+
+    /**
+     * Deletes current TopicType selected
+     */
+    @NotifyChange({"selected", "topicTypes"})
+    @Command
+    public void deleteTopicType() {
+        getTopicTypeService().deleteTopicType(selected);
+        deleteFromList();
+    }
+
+    /**
+     * Deletes from list of TopicTypes on web-form.
+     */
+    @NotifyChange({"selected", "topicTypes"})
+    @Command
+    public void deleteFromList() {
+        getTopicTypes().remove(selected);
+        setSelected(null);
+    }
+
+    /**
+     * Collects errors obtained from {@link ValidationResult}
+     * and represents them as localized String
+     *
+     * @param result {@link ValidationResult}
+     * @return Errors as String
+     */
+    //TODO: Maybe that should be in some new bean? Like CommonValidator(Entity)
+    private String collectErrors(ValidationResult result) {
+        StringBuilder errorMessage = new StringBuilder();
+        Set<ValidationError> errors = result.getErrors();
+
+        for (Iterator<ValidationError> i = errors.iterator(); i.hasNext(); ) {
+            errorMessage.append(Labels.getLabel(i.next().getErrorMessageCode()));
+            if (i.hasNext()) {
+                errorMessage.append(", ");
+            }
+        }
+
+        return errorMessage.toString();
+    }
+
+    //validators for prompt
+
+    /**
+     * Validator for title field on web-form
+     *
+     * @return Validator for title field
+     */
+    public Validator getTitleValidator() {
+        return new AbstractValidator() {
+            public void validate(ValidationContext ctx) {
+                String title = (String) ctx.getProperty().getValue();
+                Long id = selected.getId();
+                //TopicType for test should be similar in meaningful fields
+                // to what we're going to save - (id, title)
+                TopicType test = new TopicType(title, StringUtils.EMPTY);
+                test.setId(id);
+
+                ValidationResult result = entityValidator.validate(test);
+
+                if (result.hasErrors()) {
+                    addInvalidMessage(ctx, collectErrors(result));
+                }
+            }
+        };
     }
 
     // getters & setters (you don't say!)
@@ -100,7 +221,6 @@ public class TopicTypeVM {
      *
      * @param topicTypes ListModelList<TopicType> to use
      */
-    @NotifyChange
     public void setTopicTypes(ListModelList<TopicType> topicTypes) {
         this.topicTypes = topicTypes;
     }
@@ -119,7 +239,6 @@ public class TopicTypeVM {
      *
      * @param selected is TopicType for select
      */
-    @NotifyChange
     public void setSelected(TopicType selected) {
         this.selected = selected;
     }
@@ -142,94 +261,13 @@ public class TopicTypeVM {
         this.topicTypeService = topicTypeService;
     }
 
-    //action command
-    @NotifyChange({"selected", "topicTypes"})
-    @Command
     /**
-     * Creates new TopicType and adds it on form
-     */
-    public void newTopicType() {
-        TopicType topicType = new TopicType();
-        //select the new one
-        selected = topicType;
-        selected.setTitle("New Title");
-        selected.setDescription("New Description");
-        getTopicTypes().add(topicType);
-    }
-
-    @NotifyChange("selected")
-    @Command
-    /**
-     * Saves current TopicType selected. Doesn't save other if changed.
-     * Shows warning or error messages if something is wrong
-     */
-    public void saveTopicType() {
-        getTopicTypeService().saveOrUpdate(selected);
-    }
-
-    @NotifyChange({"selected", "topicTypes"})
-    @Command
-    /**
-     * Deletes current TopicType selected
-     */
-    public void deleteTopicType() {
-        getTopicTypeService().deleteTopicType(selected);
-        deleteFromList();
-    }
-
-    @NotifyChange({"selected", "topicTypes"})
-    @Command
-    /**
-     * Deletes from list of TopicTypes on web-form.
-     */
-    public void deleteFromList() {
-        getTopicTypes().remove(selected);
-        setSelected(null);
-    }
-
-    /**
-     * Collects errors obtained from {@link ValidationResult}
-     * and represents them as localized String
+     * Returns message shown when window for create or
+     * edit is opened.
      *
-     * @param result {@link ValidationResult}
-     * @return Errors as String
+     * @return message as {@link String}
      */
-    //TODO: Maybe that should be in some new bean? Like CommonValidator(Entity)
-    private String collectErrors(ValidationResult result) {
-        StringBuilder errorMessage = new StringBuilder();
-        Set<ValidationError> errors = result.getErrors();
-
-        for (Iterator<ValidationError> i = errors.iterator(); i.hasNext(); ) {
-            errorMessage.append(Labels.getLabel(i.next().getErrorMessageCode()));
-            if (i.hasNext()) {
-                errorMessage.append(", ");
-            }
-        }
-        ;
-
-        return errorMessage.toString();
+    public String getEditMessage() {
+        return editMessage;
     }
-
-    //validators for prompt
-
-    /**
-     * Validator for title field on web-form
-     *
-     * @return Validator for title field
-     */
-    public Validator getTitleValidator() {
-        return new AbstractValidator() {
-            public void validate(ValidationContext ctx) {
-                String title = (String) ctx.getProperty().getValue();
-                TopicType test = new TopicType(title, StringUtils.EMPTY);
-
-                ValidationResult result = entityValidator.validate(test);
-
-                if (result.hasErrors()) {
-                    addInvalidMessage(ctx, collectErrors(result));
-                }
-            }
-        };
-    }
-
 }
