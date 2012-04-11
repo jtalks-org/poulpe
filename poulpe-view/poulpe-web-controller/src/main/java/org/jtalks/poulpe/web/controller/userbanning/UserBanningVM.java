34025b2d054549508150e5b2adcc30c8c6140038
ctapobep <ctapobep@javatalks.ru>
2012-04-11 19:34:32 +0200

    Tabs were replaced with spaces in UserBanningVM

--- poulpe-view/poulpe-web-controller/src/main/java/org/jtalks/poulpe/web/controller/userbanning/UserBanningVM.java
+++ poulpe-view/poulpe-web-controller/src/main/java/org/jtalks/poulpe/web/controller/userbanning/UserBanningVM.java
@@ -1,75 +1,74 @@
 package org.jtalks.poulpe.web.controller.userbanning;
 
-import javax.annotation.Nonnull;
-
 import org.jtalks.poulpe.model.entity.User;
 import org.jtalks.poulpe.service.UserService;
+import org.zkoss.bind.annotation.BindingParam;
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zkplus.databind.BindingListModelList;
 import org.zkoss.zul.ListModelList;
 import org.zkoss.zul.Window;
-import org.zkoss.bind.annotation.*;
-import org.zkoss.zkplus.databind.BindingListModelList;
 
+import javax.annotation.Nonnull;
 import java.util.ArrayList;
 
 public class UserBanningVM {
-	@Wire
-	private Window customersWindow;
-	private UserService userService;
+    @Wire
+    private Window customersWindow;
+    private UserService userService;
 
-	private User selectedUser;
-	private ListModelList<User> bannedUsers = new BindingListModelList<User>(
-			new ArrayList<User>(), true);
+    private User selectedUser;
+    private ListModelList<User> bannedUsers = new BindingListModelList<User>(
+            new ArrayList<User>(), true);
 
-	public UserBanningVM(UserService userService) {
-		this.userService = userService;
-		initData();
-	}
+    public UserBanningVM(UserService userService) {
+        this.userService = userService;
+        initData();
+    }
 
-	public void initData() {
+    public void initData() {
 
-		bannedUsers.addAll(userService.getAllBannedUsers());
-	}
+        bannedUsers.addAll(userService.getAllBannedUsers());
+    }
 
-	@Command
-	public void addBannedUser() {
+    @Command
+    public void addBannedUser() {
 
-		openEditUserDialog(customersWindow);
-	}
+        openEditUserDialog(customersWindow);
+    }
 
-	@Command
-	public void editBannedUser(@Nonnull @BindingParam("user") User user) {
-		this.selectedUser = user;
-		openEditUserDialog(customersWindow);
-	}
+    @Command
+    public void editBannedUser(@Nonnull @BindingParam("user") User user) {
+        this.selectedUser = user;
+        openEditUserDialog(customersWindow);
+    }
 
-	private Component openEditUserDialog(Window customersWindow) {
-		return Executions.createComponents(
-				"/WEB-INF/pages/edit_banned_user.zul", customersWindow, null);
-	}
+    private Component openEditUserDialog(Window customersWindow) {
+        return Executions.createComponents(
+                "/WEB-INF/pages/edit_banned_user.zul", customersWindow, null);
+    }
 
-	@Command
-	public void deleteBannedUser() {
+    @Command
+    public void deleteBannedUser() {
 
-	}
+    }
 
-	public User getSelectedUser() {
-		return selectedUser;
-	}
+    public User getSelectedUser() {
+        return selectedUser;
+    }
 
-	public void setSelectedUser(User selectedUser) {
-		this.selectedUser = selectedUser;
-	}
+    public void setSelectedUser(User selectedUser) {
+        this.selectedUser = selectedUser;
+    }
 
-	public ListModelList<User> getBannedUsers() {
-		return bannedUsers;
-	}
+    public ListModelList<User> getBannedUsers() {
+        return bannedUsers;
+    }
 
-	public void setBannedUsers(ListModelList<User> bannedUsers) {
-		this.bannedUsers = bannedUsers;
-	}
+    public void setBannedUsers(ListModelList<User> bannedUsers) {
+        this.bannedUsers = bannedUsers;
+    }
 
 }
