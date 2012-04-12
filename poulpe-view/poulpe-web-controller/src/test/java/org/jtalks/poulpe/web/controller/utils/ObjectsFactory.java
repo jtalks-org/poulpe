c34759aae7c09fcfde3a35fa7440e68cf3c26201
Zhivaev Vyacheslav <zhslma@gmail.com>
2012-04-12 13:27:59 +0400

    #POULPE-179
    * added tests for EditGroupsForComponentPermissionVm
    * minor fixes

--- poulpe-view/poulpe-web-controller/src/test/java/org/jtalks/poulpe/web/controller/utils/ObjectsFactory.java
+++ poulpe-view/poulpe-web-controller/src/test/java/org/jtalks/poulpe/web/controller/utils/ObjectsFactory.java
@@ -27,6 +27,7 @@ import org.jtalks.poulpe.model.entity.PoulpeGroup;
 import org.jtalks.poulpe.model.entity.PoulpeSection;
 import org.jtalks.poulpe.model.entity.TopicType;
 import org.jtalks.poulpe.model.entity.User;
+import org.jtalks.poulpe.web.controller.SelectedEntity;
 
 import com.google.common.collect.Lists;
 
@@ -126,7 +127,7 @@ public class ObjectsFactory {
 
     public static List<PoulpeGroup> fakeGroupList(int listSize) {
         List<PoulpeGroup> result = Lists.newArrayList();
-        
+
         if (listSize > 0) {
             for (int i = 0; i < listSize; i++) {
                 result.add(fakeGroup());
@@ -141,8 +142,20 @@ public class ObjectsFactory {
     }
 
     public static Jcommune fakeForum() {
-    	Jcommune forum = new Jcommune();
-    	forum.setSections(fakeSections());
-    	return forum;
+        Jcommune forum = new Jcommune();
+        forum.setSections(fakeSections());
+        return forum;
+    }
+
+    /**
+     * Create {@link SelectedEntity} instance with predefined internal {@code SelectedEntity.entity}.
+     * 
+     * @param entity the entity to set
+     * @return new instance of {@link SelectedEntity}
+     */
+    public static <T> SelectedEntity<T> createSelectedEntity(T entity) {
+        SelectedEntity<T> result = new SelectedEntity<T>();
+        result.setEntity(entity);
+        return result;
     }
 }
