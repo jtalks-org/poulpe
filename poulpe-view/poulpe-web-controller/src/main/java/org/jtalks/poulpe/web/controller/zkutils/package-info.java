8b9622035bb2ad027910a0404888536abab48b71
ctapobep <ctapobep@javatalks.ru>
2012-05-11 20:53:02 +0200

    #POULPE-258 Added licence headers

--- poulpe-view/poulpe-web-controller/src/main/java/org/jtalks/poulpe/web/controller/zkutils/package-info.java
+++ poulpe-view/poulpe-web-controller/src/main/java/org/jtalks/poulpe/web/controller/zkutils/package-info.java
@@ -1,4 +1,18 @@
 /**
+ * Copyright (C) 2011  JTalks.org Team
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+/**
  * Contains utilities and helpers to work with classes of ZK framework. If you have some generic-purpose class to work
  * with ZK that can be reused by others, place this class in this package.
  */
