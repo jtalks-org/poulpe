b12857ec47a13ca90896ba33db71f3a6a0775254
Guram Savinov <guram.savinov@gmail.com>
2012-06-03 21:59:50 +0400

    #POULPE-238 Added test for PoulpeSection covered adding branch to the specified index Refactored old tests in PoulpeSection

--- poulpe-model/src/test/java/org/jtalks/poulpe/model/entity/PoulpeSectionTest.java
+++ poulpe-model/src/test/java/org/jtalks/poulpe/model/entity/PoulpeSectionTest.java
@@ -9,31 +9,60 @@ import static org.testng.AssertJUnit.*;
 
 /**
  * @author stanislav bashkirtsev
+ * @author Guram Savinov
  */
 public class PoulpeSectionTest {
     @Test(dataProvider = "provideFilledSection")
-    public void testAddBranchIfAbsent(PoulpeSection section) throws Exception {
+    public void testAddBranchIfAbsent(PoulpeSection section) {
         int originalSize = section.getAmountOfBranches();
         PoulpeBranch branchToAdd = new PoulpeBranch("", "");
-        section.addOrUpdateBranch(branchToAdd);
-        assertEquals(section.getAmountOfBranches(), originalSize + 1);
-        assertSame(section.getBranch(originalSize), branchToAdd);
+
+        section.addBranchIfAbsent(branchToAdd);
+        assertEquals(originalSize + 1, section.getAmountOfBranches());
+        assertSame(branchToAdd, section.getBranch(originalSize));
+        assertSame(section, branchToAdd.getPoulpeSection());
     }
 
     @Test(dataProvider = "provideFilledSection")
-    public void testAddBranchIfAbsent_withAlreadyPresent(PoulpeSection section) throws Exception {
+    public void testAddBranchIfAbsent_withNull(PoulpeSection section) {
+        int originalBranchSize = section.getAmountOfBranches();
+
+        section.addBranchIfAbsent(null);
+        assertEquals(originalBranchSize, section.getAmountOfBranches());
+    }
+
+    @Test(dataProvider = "provideFilledSection")
+    public void testAddBranchIfAbsent_withAlreadyPresent(PoulpeSection section) {
         int originalBranchSize = section.getAmountOfBranches();
         PoulpeBranch branchToAdd = createEqualBranch(section.getBranch(0));
 
         section.addBranchIfAbsent(branchToAdd);
-        assertEquals(section.getAmountOfBranches(), originalBranchSize);
-        assertNotSame(section.getBranch(0), branchToAdd);
+        assertEquals(originalBranchSize, section.getAmountOfBranches());
+        assertNotSame(branchToAdd, section.getBranch(0));
     }
 
     @Test(dataProvider = "provideFilledSection")
-    public void testContainBranch(PoulpeSection section) throws Exception {
+    public void testContainBranch(PoulpeSection section) {
         assertFalse(section.containsBranch(new PoulpeBranch()));
         assertTrue(section.containsBranch(createEqualBranch(section.getBranch(0))));
+    }
+    
+    @Test(dataProvider = "provideFilledSection")
+    public void testGetLastBranch(PoulpeSection section) {
+        List<PoulpeBranch> branches = section.getPoulpeBranches();
+        assertSame(branches.get(branches.size() - 1), section.getLastBranch());
+    }
+    
+    @Test(dataProvider = "provideFilledSection")
+    public void testAddBranchIfAbsentTo(PoulpeSection section) {
+        final int indexForAddedBranch = 1;
+        List<PoulpeBranch> allBranches = section.getPoulpeBranches();
+        PoulpeBranch branchToAdd = new PoulpeBranch("", "");
+        PoulpeBranch branchToShift = allBranches.get(indexForAddedBranch);
+        
+        section.addBranchIfAbsentTo(1, branchToAdd);
+        assertSame(branchToAdd, allBranches.get(indexForAddedBranch));
+        assertSame(branchToShift, allBranches.get(indexForAddedBranch + 1));
     }
 
     private PoulpeBranch createEqualBranch(PoulpeBranch branch) {
@@ -42,17 +71,12 @@ public class PoulpeSectionTest {
         return branchToAdd;
     }
 
-    @Test(dataProvider = "provideFilledSection")
-    public void testGetLastBranch(PoulpeSection section) {
-        List<PoulpeBranch> branches = section.getPoulpeBranches();
-        assertSame(branches.get(branches.size() - 1), section.getLastBranch());
-    }
-
     @DataProvider
     public Object[][] provideFilledSection() {
         PoulpeSection section = new PoulpeSection("section-name", "section-description");
         section.getBranches().add(new PoulpeBranch("branch-name1"));
         section.getBranches().add(new PoulpeBranch("branch-name2"));
+        section.getBranches().add(new PoulpeBranch("branch-name3"));
         return new Object[][]{{section}};
     }
 }
