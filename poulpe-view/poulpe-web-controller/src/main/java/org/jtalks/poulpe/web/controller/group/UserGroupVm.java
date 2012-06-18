803c7fa59d115f5d74bc36697bc16f26e929a99d
gesser <gesser@javatalks.ru>
2012-06-17 23:51:22 -0400

    #POULPE-320 Method getByName return empty group list if search string are empty.

--- poulpe-view/poulpe-web-controller/src/main/java/org/jtalks/poulpe/web/controller/group/UserGroupVm.java
+++ poulpe-view/poulpe-web-controller/src/main/java/org/jtalks/poulpe/web/controller/group/UserGroupVm.java
@@ -14,6 +14,7 @@
  */
 package org.jtalks.poulpe.web.controller.group;
 
+import org.apache.commons.lang.StringUtils;
 import org.jtalks.common.model.entity.Group;
 import org.jtalks.poulpe.model.entity.PoulpeBranch;
 import org.jtalks.poulpe.service.GroupService;
@@ -84,7 +85,11 @@ public class UserGroupVm {
     @Command
     public void searchGroup() {
         groups.clear();
-        groups.addAll(groupService.getByName(searchString));
+        if ( StringUtils.isNotBlank(this.searchString)) {
+        	groups.addAll(groupService.getByName(searchString));
+        } else {
+        	groups.addAll(groupService.getAll());
+        }
     }
 
     /**
