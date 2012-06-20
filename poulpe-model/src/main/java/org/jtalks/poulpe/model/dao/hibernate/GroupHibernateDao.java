dfc2c8a0add7a0fbadb46ddae0967d8ddbacd449
gesser <gesser@javatalks.ru>
2012-06-19 23:07:54 -0400

    #POULPE-320 Searching groups returns no results when empty string in specified. Created named query for searching groups by name using like statement. Formatting applied.

--- poulpe-model/src/main/java/org/jtalks/poulpe/model/dao/hibernate/GroupHibernateDao.java
+++ poulpe-model/src/main/java/org/jtalks/poulpe/model/dao/hibernate/GroupHibernateDao.java
@@ -26,59 +26,63 @@ import java.util.List;
 
 /**
  * Hibernate implementation of {@link GroupDao}
- *
+ * 
  * @author Vitaliy Kravchenko
  * @author Pavel Vervenko
  */
-public class GroupHibernateDao extends AbstractHibernateParentRepository<Group> implements GroupDao {
-    private final static String MODERAING_GROUP_ID = "moderating_group_id";
+public class GroupHibernateDao extends AbstractHibernateParentRepository<Group>
+		implements GroupDao {
+	private final static String MODERAING_GROUP_ID = "moderating_group_id";
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    @SuppressWarnings("unchecked")
-    public List<Group> getAll() {
-        return getSession().createQuery("from Group").list();
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	@SuppressWarnings("unchecked")
+	public List<Group> getAll() {
+		return getSession().createQuery("from Group").list();
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @SuppressWarnings("unchecked")
-    @Override
-    public List<Group> getByName(String name) {
-        Assert.throwIfNull(name, "name");
-        if (StringUtils.isNotBlank(name)) {
-	        Query query = getSession().createQuery("from Group g where g.name = ?");
-	        query.setString(0, name);
-	        return query.list();
-        }
-        return this.getAll();
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Group> getByName(String name) {
+		Assert.throwIfNull(name, "name");
+		if (StringUtils.isBlank(name)) {
+			return this.getAll();
+		}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void delete(Group group) {
-        getSession().update(group);
+		Query query = getSession().getNamedQuery("findGroupByName");
+		query.setString("name", name);
+		return query.list();
+	}
 
-        group.getUsers().clear();
-        saveOrUpdate(group);
-        super.delete(group);
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void delete(Group group) {
+		getSession().update(group);
 
-    /**
-     * {@inheritDoc}
-     */
-    @SuppressWarnings("unchecked")
-    @Override
-    public List<PoulpeBranch> getModeratingBranches(Group group) {
-        getSession().flush();
-        Query query = getSession().createQuery("from PoulpeBranch where MODERATORS_GROUP_ID=:" + MODERAING_GROUP_ID);
-        query.setParameter(MODERAING_GROUP_ID, group.getId());
-        return query.list();
-    }
+		group.getUsers().clear();
+		saveOrUpdate(group);
+		super.delete(group);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<PoulpeBranch> getModeratingBranches(Group group) {
+		getSession().flush();
+		Query query = getSession().createQuery(
+				"from PoulpeBranch where MODERATORS_GROUP_ID=:"
+						+ MODERAING_GROUP_ID);
+		query.setParameter(MODERAING_GROUP_ID, group.getId());
+		return query.list();
+	}
 
 }
