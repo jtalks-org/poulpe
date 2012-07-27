1272fbe737d29e4e164a0206d50f7e9b593bd72a
ctapobep <ctapobep@javatalks.ru>
2012-07-27 07:53:52 +0100

    #POULPE-359 * Replaced logic from constructor, used @Init instead (constructor should not include logic) * Changed a scope of the VM's bean to Desktop instead of singleton that would be shared across all the users What's next: * ZUL page should use DataBinding instead of ${} in order VM to work correctly * Unit tests should be written

--- poulpe-view/poulpe-web-controller/src/main/java/org/jtalks/poulpe/web/controller/users/UserPersonalPermissionsVm.java
+++ poulpe-view/poulpe-web-controller/src/main/java/org/jtalks/poulpe/web/controller/users/UserPersonalPermissionsVm.java
@@ -1,29 +1,18 @@
 package org.jtalks.poulpe.web.controller.users;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-
-import javax.annotation.Nonnull;
-
-import org.jtalks.common.model.entity.Entity;
-import org.jtalks.common.model.permissions.GeneralPermission;
-import org.jtalks.common.model.permissions.JtalksPermission;
-import org.jtalks.poulpe.model.dto.PermissionForEntity;
-import org.jtalks.poulpe.model.dto.PermissionsMap;
-import org.jtalks.poulpe.model.entity.Component;
-import org.jtalks.poulpe.service.ComponentService;
-import org.jtalks.poulpe.web.controller.SelectedEntity;
-import org.jtalks.poulpe.web.controller.WindowManager;
-import org.jtalks.poulpe.web.controller.zkmacro.EntityPermissionsBlock;
-import org.jtalks.poulpe.web.controller.zkmacro.PermissionManagementBlock;
-import org.zkoss.bind.annotation.BindingParam;
-import org.zkoss.bind.annotation.Command;
-import org.zkoss.util.resource.Labels;
-
 import com.google.common.collect.Lists;
+import org.jtalks.common.model.entity.Entity;
 import org.jtalks.common.model.entity.Group;
 import org.jtalks.common.model.permissions.ProfilePermission;
+import org.jtalks.poulpe.model.dto.PermissionsMap;
+import org.jtalks.poulpe.web.controller.zkmacro.EntityPermissionsBlock;
+import org.jtalks.poulpe.web.controller.zkmacro.PermissionManagementBlock;
+import org.zkoss.bind.annotation.Init;
+import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.util.resource.Labels;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Works with user permissions like editing own profile or sending private messages. These permissions might be
@@ -33,37 +22,30 @@ import org.jtalks.common.model.permissions.ProfilePermission;
  * @author stanislav bashkirtsev
  */
 public class UserPersonalPermissionsVm {
+    private final List<EntityPermissionsBlock> blocks = Lists.newArrayList();
 
-    // Internal state
-    private final List<EntityPermissionsBlock> blocks;
-
-    public UserPersonalPermissionsVm() {
-    
-        blocks = Lists.newArrayList();
-        updateView();
-    }
-    
-    public List<EntityPermissionsBlock> getBlocks() {
-        return Collections.unmodifiableList(blocks);
-    }
     /**
      * Update VM state.
      */
-    private void updateView() {
-       blocks.clear();
-       List<PermissionManagementBlock> blocks_in=Lists.newArrayList();
-       List<ProfilePermission> permissions = ProfilePermission.getAllAsList();
-       PermissionsMap<ProfilePermission> permissionsMap = new PermissionsMap<ProfilePermission>(permissions); 
+    @Init
+    public void updateView() {
+        blocks.clear();
+        List<PermissionManagementBlock> blocksIn = Lists.newArrayList();
+        List<ProfilePermission> permissions = ProfilePermission.getAllAsList();
+        PermissionsMap<ProfilePermission> permissionsMap = new PermissionsMap<ProfilePermission>(permissions);
 
-        for (ProfilePermission permission : permissions) {            
-        blocks_in.add(new PermissionManagementBlock(permission, permissionsMap, Labels
-                     .getLabel("permissions.allow_label"), Labels.getLabel("permissions.restrict_label")));        
+        for (ProfilePermission permission : permissions) {
+            blocksIn.add(new PermissionManagementBlock(permission, permissionsMap, Labels
+                    .getLabel("permissions.allow_label"), Labels.getLabel("permissions.restrict_label")));
         }
-       
+
         Entity entity = new Group("some");
-        
-        blocks.add(new EntityPermissionsBlock(entity, "111", blocks_in));
-    }    
-    
-    
+        blocks.add(new EntityPermissionsBlock(entity, "111", blocksIn));
+    }
+
+    public List<EntityPermissionsBlock> getBlocks() {
+        return blocks;
+    }
+
+
 }
