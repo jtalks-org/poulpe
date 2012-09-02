5bb77defcb9c0b8c910548ef4b18ed263a8c3c89
ctapobep <ctapobep@javatalks.ru>
2012-09-02 14:10:27 +0200

    Removed unnecessary dependencies from web-view, removing extra code from test.

--- poulpe-view/poulpe-web-controller/src/test/java/org/jtalks/poulpe/web/controller/component/AddComponentVmTest.java
+++ poulpe-view/poulpe-web-controller/src/test/java/org/jtalks/poulpe/web/controller/component/AddComponentVmTest.java
@@ -1,29 +1,19 @@
 /**
-* Copyright (C) 2011  JTalks.org Team
-* This library is free software; you can redistribute it and/or
-* modify it under the terms of the GNU Lesser General Public
-* License as published by the Free Software Foundation; either
-* version 2.1 of the License, or (at your option) any later version.
-* This library is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-* Lesser General Public License for more details.
-* You should have received a copy of the GNU Lesser General Public
-* License along with this library; if not, write to the Free Software
-* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+ * Copyright (C) 2011  JTalks.org Team
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 package org.jtalks.poulpe.web.controller.component;
 
-import static org.mockito.Matchers.argThat;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.hamcrest.BaseMatcher;
-import org.hamcrest.Description;
-import org.jtalks.poulpe.model.entity.ComponentBase;
-import org.jtalks.poulpe.model.entity.Component;
-import org.jtalks.poulpe.model.entity.ComponentType;
 import org.jtalks.poulpe.service.ComponentService;
 import org.jtalks.poulpe.web.controller.SelectedEntity;
 import org.jtalks.poulpe.web.controller.WindowManager;
@@ -33,6 +23,8 @@ import org.mockito.MockitoAnnotations;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import static org.mockito.Mockito.verify;
+
 /**
  * @author Alexey Grigorev
  * @author Kazantcev Leonid
@@ -41,8 +33,10 @@ public class AddComponentVmTest {
 
     AddComponentVm addComponentVm;
 
-    @Mock ComponentService componentService;
-    @Mock WindowManager windowManager;
+    @Mock
+    ComponentService componentService;
+    @Mock
+    WindowManager windowManager;
     @Mock
     SelectedEntity component;
 
@@ -65,39 +59,15 @@ public class AddComponentVmTest {
         verify(windowManager).open(ComponentsVm.COMPONENTS_PAGE_LOCATION);
     }
 
-    private void givenBaseComponents() {
-        when(componentService.baseComponentFor(ComponentType.FORUM)).thenReturn(
-                new ComponentBase(ComponentType.FORUM));
-        when(componentService.baseComponentFor(ComponentType.ADMIN_PANEL)).thenReturn(
-                new ComponentBase(ComponentType.ADMIN_PANEL));
-        when(componentService.baseComponentFor(ComponentType.ARTICLE)).thenReturn(
-                new ComponentBase(ComponentType.ARTICLE));
-    }
-
-
     @Test
     public void getAvailableComponentTypes() {
         addComponentVm.getAvailableComponentTypes();
         verify(componentService).getAvailableTypes();
     }
-    
+
     @Test
     public void openWindowForAdding() {
         AddComponentVm.openWindowForAdding(windowManager);
         verify(windowManager).open(AddComponentVm.ADD_COMPONENT_LOCATION);
-    }
-
-    private static Component fieldsEqualTo(final Component expected) {
-        return argThat(new BaseMatcher<Component>() {
-            @Override
-            public boolean matches(Object o) {
-                return EqualsBuilder.reflectionEquals(o, expected, "uuid");
-            }
-
-            @Override
-            public void describeTo(Description d) {
-                d.appendText(expected.toString());
-            }
-        });
     }
 }
