bb5cdb38e17a12652556f5ceb991fafeb1561c8b
lamao <lamao@yandex.ru>
2013-02-25 14:09:18 +0200

    #POULPE-497. modifying comments

--- poulpe-model/src/main/resources/org/jtalks/poulpe/migrations/V19__Reconcile_PROPERTIES_table_with_common_scripts.sql
+++ poulpe-model/src/main/resources/org/jtalks/poulpe/migrations/V19__Reconcile_PROPERTIES_table_with_common_scripts.sql
@@ -1,5 +1,20 @@
-/* Poulpe in previous migrations touches PROPERTIES table which lays in Common
- * scope. This script modify this table to reconcile it columns with 
- * definitions from common.  
- */
+--
+-- Copyright (C) 2011  JTalks.org Team
+-- This library is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU Lesser General Public
+-- License as published by the Free Software Foundation; either
+-- version 2.1 of the License, or (at your option) any later version.
+-- This library is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+-- Lesser General Public License for more details.
+-- You should have received a copy of the GNU Lesser General Public
+-- License along with this library; if not, write to the Free Software
+-- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+--
+
+-- Poulpe in previous migrations touched PROPERTIES table which lays in Common
+-- scope. This script modify this table to reconcile it columns with 
+-- definitions from common.  
+
 ALTER TABLE PROPERTIES MODIFY VALIDATION_RULE VARCHAR(255) NOT NULL DEFAULT '';
