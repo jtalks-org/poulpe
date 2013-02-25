cac8965792ad78479d7043909c3cd0ca8c4ea802
Mikhail Zaitsev <flashed@mail.ru>
2013-02-25 22:47:44 +0400

    POULPE-448 Changes...

---
+++ poulpe-model/src/main/java/org/jtalks/poulpe/model/sorting/AbstractSortingRequest.java
@@ -0,0 +1,66 @@
+package org.jtalks.poulpe.model.sorting;
+
+import org.jtalks.poulpe.model.pages.Pagination;
+
+/**
+ * Abstract class of request with parameters of sorting.
+ */
+public abstract class AbstractSortingRequest {
+
+    /**
+     * Soring by ascending
+     */
+    private boolean ascending = true;
+
+    /**
+     * Pagination of request
+     */
+    private Pagination pagination;
+
+    /**
+     * Column for sorting
+     */
+    private String column;
+
+    /**
+     * @return sorting by ascending or not
+     */
+    public boolean isAscending() {
+        return ascending;
+    }
+
+    /**
+     * @param ascending sorting by ascending or not
+     */
+    public void setAscending(boolean ascending) {
+        this.ascending = ascending;
+    }
+
+    /**
+     * @return pagination of request sorting
+     */
+    public Pagination getPagination() {
+        return pagination;
+    }
+
+    /**
+     * @param pagination pagination of request sorting
+     */
+    public void setPagination(Pagination pagination) {
+        this.pagination = pagination;
+    }
+
+    /**
+     * @return column that will be sorted
+     */
+    public String getColumn() {
+        return column;
+    }
+
+    /**
+     * @param column column that will be sorted
+     */
+    public void setColumn(String column) {
+        this.column = column;
+    }
+}
---
+++ poulpe-model/src/main/java/org/jtalks/poulpe/model/sorting/UsersSortingRequest.java
@@ -0,0 +1,18 @@
+package org.jtalks.poulpe.model.sorting;
+
+/**
+ * Request with parameters of sorting for users.
+ */
+public class UsersSortingRequest  extends AbstractSortingRequest{
+
+    /**
+     * Sorting by USERNAME
+     */
+    public String BY_USERNAME = "USERNAME";
+
+    /**
+     * Sorting by EMAIL
+     */
+    public String BY_EMAIL = "EMAIL";
+
+}
