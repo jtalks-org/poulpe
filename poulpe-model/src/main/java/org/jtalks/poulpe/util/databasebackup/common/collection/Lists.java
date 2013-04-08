ab0e99fe8eaf950ed632614f63731fde2624245b
Evgeny Surovtsev <eugene.sourovtsev@gmail.com>
2013-04-08 16:18:53 +0200

    dbbackup - javadocs and unittests for common.collection package

--- poulpe-model/src/main/java/org/jtalks/poulpe/util/databasebackup/common/collection/Lists.java
+++ poulpe-model/src/main/java/org/jtalks/poulpe/util/databasebackup/common/collection/Lists.java
@@ -3,19 +3,40 @@ package org.jtalks.poulpe.util.databasebackup.common.collection;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Static utility methods pertaining to List instances. Also see the class's counterparts {@link Sets} and {@link Maps}.
+ * 
+ * @author Evgeny Surovtsev
+ * 
+ */
 public final class Lists {
+    /**
+     * Creation an instance of the class is forbidden.
+     */
     private Lists() {
 
     }
 
-    public static final <T> List<T> newArrayList() {
+    /**
+     * Creates a mutable, empty ArrayList instance.
+     * 
+     * @return an empty ArrayList instance.
+     */
+    public static <T> List<T> newArrayList() {
         return new ArrayList<T>();
     }
 
-    public static final <T> List<T> newArrayList(T... items) {
+    /**
+     * Creates a mutable ArrayList instance containing the given elements.
+     * 
+     * @param elements
+     *            the elements that the list should contain, in order
+     * @return a new ArrayList containing those elements
+     */
+    public static <T> List<T> newArrayList(T... elements) {
         List<T> result = new ArrayList<T>();
-        for (T item : items) {
-            result.add(item);
+        for (T element : elements) {
+            result.add(element);
         }
         return result;
     }
