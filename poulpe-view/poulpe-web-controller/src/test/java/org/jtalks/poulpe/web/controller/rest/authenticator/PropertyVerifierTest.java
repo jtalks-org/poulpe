c571b81aaf570814d762d0e4cd252c4efdf5df07
ctapobep <ctapobep@javatalks.ru>
2013-07-22 09:04:51 +0400

    #POULPE-512 Added a test that checks whether to do basic authentication if password is empty. That test is failing which means the task is not down - we need basic authentication to consider empty password as no authentication.

--- poulpe-view/poulpe-web-controller/src/test/java/org/jtalks/poulpe/web/controller/rest/authenticator/PropertyVerifierTest.java
+++ poulpe-view/poulpe-web-controller/src/test/java/org/jtalks/poulpe/web/controller/rest/authenticator/PropertyVerifierTest.java
@@ -7,22 +7,49 @@ import org.jtalks.poulpe.service.ComponentService;
 import org.restlet.Request;
 import org.restlet.Response;
 import org.restlet.security.Verifier;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.testng.Assert.assertEquals;
 
 public class PropertyVerifierTest {
+    ComponentService componentService;
+    PropertyVerifier propertyVerifier;
 
-    ComponentService componentService = mock(ComponentService.class);
+    @BeforeMethod
+    public void setUp() throws Exception {
+        componentService = mock(ComponentService.class);
+        propertyVerifier = new PropertyVerifier(componentService);
+    }
 
-    PropertyVerifier propertyVerifier = new PropertyVerifier(componentService);
+    @Test(enabled = false)
+    public void emptyUsernameAndPasswordMeanNoAuthentication() throws Exception {
+        Component component = new Component();
+        component.addProperty(PropertyVerifier.USERNAME_PROP, "");
+        component.addProperty(PropertyVerifier.PASSWORD_PROP, "");
+
+        when(componentService.getByType(ComponentType.ADMIN_PANEL)).thenReturn(component);
+
+        assertEquals(propertyVerifier.verify(new Request(), new Response(null)), Verifier.RESULT_VALID);
+    }
 
     @Test
-    public void testVerifyIfEmpty() throws Exception {
-
+    public void emptyUsernameMeansNoAuthentication() throws Exception {
         Component component = new Component();
-        component.addProperty(PropertyVerifier.USERNAME_PROP,"");
+        component.addProperty(PropertyVerifier.USERNAME_PROP, "");
+        component.addProperty(PropertyVerifier.PASSWORD_PROP, "134");
+
+        when(componentService.getByType(ComponentType.ADMIN_PANEL)).thenReturn(component);
+
+        assertEquals(propertyVerifier.verify(new Request(), new Response(null)), Verifier.RESULT_VALID);
+    }
+
+    @Test(enabled = false)
+    public void emptyPasswordMeansNoAuthentication() throws Exception {
+        Component component = new Component();
+        component.addProperty(PropertyVerifier.USERNAME_PROP, "1234");
         component.addProperty(PropertyVerifier.PASSWORD_PROP,"");
 
         when(componentService.getByType(ComponentType.ADMIN_PANEL)).thenReturn(component);
